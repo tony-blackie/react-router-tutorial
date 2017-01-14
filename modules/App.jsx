0afaf6c26843a232933ace1e74c8b62547f538dd
MacBook <jim.casey321@gmail.com>
2017-01-14 23:45:08 +0200

    Fix missing Link element injection

--- modules/App.jsx
+++ modules/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router';
 
 export default React.createClass({
   render() {
-    return <div>Hello</div>
+    return (
+        <ul>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/repos">Repos</Link>
+          </li>
+        </ul>
+    );
   }
 })
