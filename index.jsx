e9fb6a4212f516b8787f175c83310a8c7f5fd5de
MacBook <jim.casey321@gmail.com>
2017-01-15 14:46:05 +0200

    Add index route for root

--- index.jsx
+++ index.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
+import {
+    Router,
+    Route,
+    hashHistory,
+    IndexRoute
+} from 'react-router'
 import App from './modules/App.jsx'
 import Repos from './modules/Repos.jsx'
 import About from './modules/About.jsx'
 import Repo from './modules/Repo.jsx'
+import Home from './modules/Home.jsx'
 
 render(
     <Router history={hashHistory}>
         <Route path="/" component={App}>
+            <IndexRoute component={Home}/>
             <Route path="/repos" component={Repos} />
             <Route path="/repos/:userName/:repoName" component={Repo} />
             <Route path="/about" component={About} />
