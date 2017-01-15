fed17a0fcfa83f3d40b1ef9aed7d71691769d063
MacBook <jim.casey321@gmail.com>
2017-01-15 16:14:21 +0200

    Add dynamic url generation with form

--- modules/Repos.jsx
+++ modules/Repos.jsx
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import NavLink from './NavLink.jsx';
+import { browserHistory } from 'react-router';
 
 export default class Repos extends Component {
+
+    contextTypes: {
+        router: React.PropTypes.object
+    };
+
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const userName = event.target.elements[0].value;
+        const repo = event.target.elements[1].value;
+        const path = `/repos/${userName}/${repo}`;
+        browserHistory.push(path);
+    }
+
     render() {
         return (
             <div>
@@ -9,12 +29,20 @@ export default class Repos extends Component {
 
                 <ul>
                     <li>
-                        <Link to="repos/react-js/react-router">React Router</Link>
+                        <NavLink to="repos/react-js/react-router">React Router</NavLink>
                     </li>
                     <li>
-                        <Link to="repos/facebook/react">React</Link>
+                        <NavLink to="repos/facebook/react">React</NavLink>
+                    </li>
+                    <li>
+                        <form onSubmit={this.handleSubmit}>
+                            <input type="text" placeholder="userName"/>
+                            <input type="text" placeholder="repo"/>
+                            <button type="submit">Go</button>
+                        </form>
                     </li>
                 </ul>
+                {this.props.children}
             </div>
 
         );
