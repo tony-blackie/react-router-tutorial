2cb3492705592aea5955792932b012cdd05035b7
MacBook <jim.casey321@gmail.com>
2017-01-15 12:55:13 +0200

    Add source maps and config for dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  devtool: 'source-map',
+
   entry: './index.jsx',
 
   output: {
@@ -10,5 +12,10 @@ module.exports = {
     loaders: [
       { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
     ]
+  },
+
+  devServer: {
+    inline: true,
+    stats: 'error-only'
   }
-}
+};
