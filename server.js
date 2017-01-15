4f536486387346b5da23feb140c06de69f9f9706
MacBook <jim.casey321@gmail.com>
2017-01-15 15:40:24 +0200

    move static files to public folder

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ var path = require('path');
 
 var app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname, 'public'));
 
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-    console.log('asdasd');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 var PORT = process.env.PORT || 8080;
